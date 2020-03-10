00de936ed6f3dd84b113b2edc109a9277906a1c1
charliepan <hsinchih.pan@gmail.com>
2020-03-10 17:16:02 -0500

    Added some code for app-8 but not working will review later

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Will take a look later...
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lukeSkywalker: ""
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://swapi.co/api/people/1").then(response => {
+      this.setState({
+        lukeSkywalker: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name: {this.state.lukeSkywalker.name}</h1>
+        <h1>Birth Year: {this.state.lukeSkywalker.birth_year}</h1>
+        <h1>Height: {this.state.lukeSkywalker.height}</h1>
+        <h1>Eye Color: {this.state.lukeSkywalker.eye_color}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+// import React, { Component } from 'react'
+// import './App.css';
+// import axios from 'axios';
+
+// export default class App extends Component {
+  
+//   constructor(){
+//     super();
+//     this.state = {
+//       bulbasaur: ""
+//     }
+//   }
+
+//   getDataFromAPI(){
+//     axios.get('"https://swapi.co/api/people/1"').then(response => {
+//       this.setState({bulbasaur:response.data});
+//     });
+//   }
+  
+//   render() {
+//     console.log(this.state.bulbasaur);
+
+//     return (
+//       <div className='App'>
+//             <h1>Name: {this.state.bulbasaur.name}</h1>
+//             <h1></h1>
+//             <h1></h1>
+//       </div>
+//     )
+//   }
+// }
