8b69c436f5bff75c99933f165161e2065ac7a6b4
charliepan <hsinchih.pan@gmail.com>
2020-03-10 14:11:26 -0500

    Added code for app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      userInput: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+          <input onChange={ e =>{this.setState({userInput: e.target.value})}} type="text" name="" id=""/>
+          <h3>{this.state.userInput}</h3>
+      </div>
+    )
+  }
 }
-
-export default App;
